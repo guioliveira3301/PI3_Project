7daf332b15bec96597c4bb1abdc5ea4c3795d0ab
Aluno <aluno@naverecife.local>
2019-05-29 16:49:06 -0300

    my first commit

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+    public Image cdCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    public float gvrTimer;
+    public GameObject objectToInteract;
+
+public void GvrOn()
+{
+    gvrStatus = true;
+}
+
+public void GvrOff()
+{
+    gvrStatus = false;
+    gvrTimer = 0;
+    cdCircle.fillAmount = 0;
+}
+
+public void Input()
+{
+    objectToInteract.SetActive(true);
+}
+public void Output()
+{
+    objectToInteract.SetActive(false);
+    gvrStatus = false;
+    gvrTimer = 0;
+    cdCircle.fillAmount = 0;
+}
+    // Update is called once per frame
+    void Update()
+    {
+        if(gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            cdCircle.fillAmount = gvrTimer/totalTime;
+        }
+
+        if(gvrTimer > totalTime)
+        {
+            GVRClick.Invoke();
+        }
+    }
+
+}
